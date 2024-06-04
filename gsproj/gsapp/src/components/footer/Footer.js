9ba708055266af4ad377d7acdfddbb798e25ba8c
adelinapasculescu1 <adelina.pasculescu@student.upt.ro>
2024-06-05 00:10:03 +0300

    fronted updates and fixes

--- gsproj/gsapp/src/components/footer/Footer.js
+++ gsproj/gsapp/src/components/footer/Footer.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import './Footer.css'
-
+import { FaFacebook, FaLinkedin, FaInstagram, FaTiktok, FaMapMarkedAlt, FaPhone } from 'react-icons/fa';
+import { PiGlobe } from "react-icons/pi";
+import { CiMail } from "react-icons/ci";
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>&copy; 2024 MyCompany. All rights reserved.</p>
+            <div className = 'footer-left'>
+                <div className='container'>
+                    <div className="title">Fii la curent!</div>
+                    <div className="social-icons">
+                        <a href="https://www.upt.ro" target="_blank" rel="noopener noreferrer">
+                            <PiGlobe />
+                        </a>
+                        <a href="https://www.facebook.com/UPTimisoara" target="_blank" rel="noopener noreferrer">
+                            <FaFacebook />
+                        </a>
+                        <a href="https://www.instagram.com/upt.ro/" target="_blank" rel="noopener noreferrer">
+                            <FaInstagram />
+                        </a>
+                        <a href="https://www.tiktok.com/@upt.ro" target="_blank" rel="noopener noreferrer">
+                            <FaTiktok />
+                        </a>
+                        <a href="https://www.linkedin.com/in/Politehnica%20University%20Timisoara" target="_blank" rel="noopener noreferrer">
+                            <FaLinkedin />
+                        </a>
+                    </div>
+                </div>
+            <div className='text'>© 2024 Ghidul Studentului. Toate drepturile rezervate.</div>
+            </div>
+            <div className='footer-right'>
+                <div>
+                    <FaMapMarkedAlt /> 
+                    <div className='text'>Piaţa Victoriei Nr. 2, 300006 Timişoara, jud. Timiş, România</div>
+                </div>
+                <div >
+                    <FaPhone /> 
+                    <div className='text'>0256 - 403021</div>
+                </div>
+                <div>
+                    <CiMail />
+                    <div className='text'>rector@upt.ro</div>
+                </div>
+            </div>
         </footer>
     );
 }
--- gsproj/gsapp/src/pages/HomePage.js
+++ gsproj/gsapp/src/pages/HomePage.js
@@ -4,6 +4,9 @@ import '../App.css';
 import upt from '../assets/upt.jpg';
 import Lottie from "lottie-react";
 import home_anim from "../assets/home_anim.json";
+import home_anim2 from "../assets/home_anim2.json";
+import home_anim3 from "../assets/home_anim3.json";
+import anim_home5 from "../assets/anim_home5.json";
 
 
 const HomePage = () => {
@@ -28,7 +31,7 @@ const HomePage = () => {
                         <h1>E normal să ai întrebări.</h1>
                         <h1 className='underlined'>Aici ai și răspunsuri!</h1>
                     </div>
-                    <Lottie animationData={home_anim} />;
+                    <Lottie animationData={anim_home5} />;
             </div>
 
             {pages.map(page => (
